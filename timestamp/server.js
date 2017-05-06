a760b677518bb44e5f02ad3eab7a651c1c1a198e
Stephen Mayeux <stephenmayeux@gmail.com>
2017-05-06 17:43:51 -0500

    merge to master

--- timestamp/server.js
+++ timestamp/server.js
@@ -10,7 +10,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log('Express server running on port', port);
 })
