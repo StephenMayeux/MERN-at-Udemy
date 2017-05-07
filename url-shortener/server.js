a1680751992cb793b7d7dc3d2fa7509e2dfda744
Stephen Mayeux <stephenmayeux@gmail.com>
2017-05-06 23:08:26 -0500

    generate short urls

--- url-shortener/server.js
+++ url-shortener/server.js
@@ -2,11 +2,27 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const validator = require('validator')
+const shortid = require('shortid')
 
 mongoose.connect('mongodb://localhost/shorturl')
 
 app.get('/', (req, res) => {
   res.send('Hey hey!')
+})
+
+app.get('/*?', (req, res) => {
+  const url = req.params[0]
+  if (validator.isURL(url)) {
+    return res.send({
+      original_url: url,
+      short_url: `localhost:3000/${shortid.generate()}`
+    })
+  } else {
+    res.send({
+      success: false,
+      msg: 'Invalid URL'
+    })
+  }
 })
 
 const port = 3000
