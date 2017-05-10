f881bad9e806f2ed8c0b9f18a69668a41816ac4a
Stephen Mayeux <stephenmayeux@gmail.com>
2017-05-09 23:32:22 -0500

    move routes

---
+++ url-shortener/routes.js
@@ -0,0 +1,52 @@
+const Url = require('./models/url')
+const validator = require('validator')
+const shortid = require('shortid')
+
+module.exports = (app) => {
+
+  app.get('/', (req, res) => {
+    res.send('Hey hey!')
+  })
+
+  app.get('/goto/:short_id', (req, res) => {
+    const { short_id } = req.params
+    Url.findOne({ short_id }, (err, result) => {
+      if (err) return res.send({ success: false, msg: 'Error reading from db' })
+      if (!result) return res.send({ success: false, msg: 'Short id does not exist' })
+
+      if (result.original_url.startsWith('http')) return res.redirect(result.original_url)
+      res.redirect(`http://${result.original_url}`)
+    })
+  })
+
+  app.get('/*?', (req, res) => {
+    const url = req.params[0]
+    if (validator.isURL(url)) {
+      Url.findOne({ original_url: url }, (err, result) => {
+        if (err) return res.send({ success: false, msg: 'Error reading from db' })
+        if (result) {
+          const { original_url, short_id } = result
+          return res.send({
+            success: true,
+            original_url,
+            short_url: `localhost:3000/goto/${short_id}`
+          })
+        }
+        const newUrl = new Url({
+          original_url: url,
+          short_id: shortid.generate()
+        })
+
+        newUrl.save(err => {
+          if (err) return res.send({ success: false, msg: 'Error saving to db' })
+          res.send({ success: true, original_url: url, short_url: `localhost:3000/goto/${newUrl.short_id}` })
+        })
+      })
+    } else {
+      res.send({
+        success: false,
+        msg: 'Invalid URL'
+      })
+    }
+  })
+}
--- url-shortener/server.js
+++ url-shortener/server.js
@@ -1,58 +1,11 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const validator = require('validator')
-const shortid = require('shortid')
 
-const Url = require('./models/url')
+const routes = require('./routes')
+routes(app)
 
 mongoose.connect('mongodb://localhost/shorturl')
-
-app.get('/', (req, res) => {
-  res.send('Hey hey!')
-})
-
-app.get('/goto/:short_id', (req, res) => {
-  const { short_id } = req.params
-  Url.findOne({ short_id }, (err, result) => {
-    if (err) return res.send({ success: false, msg: 'Error reading from db' })
-    if (!result) return res.send({ success: false, msg: 'Short id does not exist' })
-
-    if (result.original_url.startsWith('http')) return res.redirect(result.original_url)
-    res.redirect(`http://${result.original_url}`)
-  })
-})
-
-app.get('/*?', (req, res) => {
-  const url = req.params[0]
-  if (validator.isURL(url)) {
-    Url.findOne({ original_url: url }, (err, result) => {
-      if (err) return res.send({ success: false, msg: 'Error reading from db' })
-      if (result) {
-        const { original_url, short_id } = result
-        return res.send({
-          success: true,
-          original_url,
-          short_url: `localhost:3000/goto/${short_id}`
-        })
-      }
-      const newUrl = new Url({
-        original_url: url,
-        short_id: shortid.generate()
-      })
-
-      newUrl.save(err => {
-        if (err) return res.send({ success: false, msg: 'Error saving to db' })
-        res.send({ success: true, original_url: url, short_url: `localhost:3000/goto/${newUrl.short_id}` })
-      })
-    })
-  } else {
-    res.send({
-      success: false,
-      msg: 'Invalid URL'
-    })
-  }
-})
 
 const port = 3000
 app.listen(port, () => {
