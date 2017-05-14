03ee3e387759a9cd20bcc2b7b0653fa7918c797a
Stephen Mayeux <stephenmayeux@gmail.com>
2017-05-13 19:11:30 -0500

    handle pagination

--- image-search/routes.js
+++ image-search/routes.js
@@ -4,7 +4,10 @@ const client = new GoogleImages(process.env.CSE_ID, process.env.API_KEY)
 module.exports = (app) => {
   app.get('/search/:term', (req, res) => {
     const { term } = req.params
-    client.search(term, { type: 'photo' })
+    const page = req.query.page ? parseInt(req.query.page, 10) : 1
+    const options = { type: 'photo', page }
+
+    client.search(term, options)
       .then(images => {
         res.send({ success: true, images })
       }).
