e6938911189222ea76cf60a288eaf7f9cee9922c
Stephen Mayeux <stephenmayeux@gmail.com>
2017-05-16 23:33:47 -0500

    set up middleware

--- voting-app/server/server.js
+++ voting-app/server/server.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
 const express = require('express')
 const app = express()
 
@@ -7,6 +10,10 @@ const routes = require('./routes')
 routes(app)
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/voting-app')
+
+app.use(cors())
+app.use(logger('combine'))
+app.use(bodyParser.json('*/*'))
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
