83ca8db22c79e17918b7c9758c17fe185c8dcc8e
Stephen Mayeux <stephenmayeux@gmail.com>
2017-05-18 11:45:41 -0500

    remove user routes

--- voting-app/server/routes/index.js
+++ voting-app/server/routes/index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.use('/polls', require('./polls'))
-router.use('/users', require('./users'))
+router.use('/polls', require('./polls'))  
 
 router.get('/', (req, res) => {
   res.send({ success: true, msg: 'Successful root route' })
