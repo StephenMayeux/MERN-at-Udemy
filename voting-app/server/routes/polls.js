9567c6f13618dafa43dad69a750ddb328e3241c1
Stephen Mayeux <stephenmayeux@gmail.com>
2017-05-18 13:08:02 -0500

    write controllers for retrieving polls

--- voting-app/server/routes/polls.js
+++ voting-app/server/routes/polls.js
@@ -3,5 +3,8 @@ const router = express.Router()
 const PollController = require('../controllers/polls')
 
 router.get('/', PollController.home)
+router.get('/all', PollController.fetchAllPolls)
+router.get('/user/:id', PollController.fetchUserPolls)
+router.get('/vote/:id', PollController.fetchOnePoll)
 
 module.exports = router
