abcdc3a9bf8b7c432200d6bdad0651321772e5a2
Stephen Mayeux <stephenmayeux@gmail.com>
2017-05-19 13:08:16 -0500

    save routes

--- voting-app/server/server.js
+++ voting-app/server/server.js
@@ -3,14 +3,21 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const logger = require('morgan')
+const jwt = require('express-jwt')
 const express = require('express')
+
 const app = express()
+const jwtCheck = jwt({
+  secret: new Buffer(process.env.APP_SECRET, 'base64'),
+  audience: process.env.APP_ID
+})
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/voting-app')
 
 app.use(cors())
 app.use(logger('combined'))
 app.use(bodyParser.json({ type: '*/*' }))
+app.use(jwtCheck.unless({ method: 'GET' }))
 
 const routes = require('./routes')
 app.use('/', routes)
