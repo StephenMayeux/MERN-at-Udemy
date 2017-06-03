3e5a9505423732c8fcb69d9651a99890a1ca2b36
Stephen Mayeux <stephenmayeux@gmail.com>
2017-06-03 12:59:59 -0500

    prevent same ip voting twice

--- voting-app/client/src/components/PollItem/index.js
+++ voting-app/client/src/components/PollItem/index.js
@@ -6,7 +6,8 @@ import {
   Panel,
   FormGroup,
   Radio,
-  Button
+  Button,
+  Modal
 } from 'react-bootstrap'
 import _ from 'lodash'
 import axios from 'axios'
@@ -16,33 +17,59 @@ const baseURL = 'http://localhost:3000'
 export default class PollItem extends Component {
   constructor(props) {
     super(props)
+
     this.state = {
       poll: null,
       hasLoaded: false,
-      selectedOption: null
+      selectedOption: null,
+      showAlertModal: false,
+      ip_address: ''
     }
+
+    this.closeModal = this.closeModal.bind(this)
+  }
+
+  closeModal() {
+    this.setState({ showAlertModal: false })
   }
 
   componentWillMount() {
-    axios.get(`${baseURL}/polls/vote/${this.props.params.id}`)
-      .then(({ data }) => {
-        this.setState({ poll: data.poll, hasLoaded: true })
-      })
+    axios.all([this.fetchPoll(), this.fetchIpAddress()])
+      .then(axios.spread((pollData, ipData) => {
+        const { poll } = pollData.data
+        const { ip } = ipData.data
+        this.setState({ poll, ip, hasLoaded: true })
+      }))
       .catch(error => {
         console.error('Error fetching single poll', error)
       })
+  }
+
+  fetchPoll() {
+    return axios.get(`${baseURL}/polls/vote/${this.props.params.id}`)
+  }
+
+  fetchIpAddress() {
+    return axios.get('https://ipapi.co/json/')
   }
 
   handleSubmit(e) {
     e.preventDefault()
-    const { _id } = this.state.poll
-    axios.post(`${baseURL}/polls/vote/${_id}`, { voter: 'test', vote: this.state.selectedOption })
-      .then(({ data }) => {
-        this.setState({ poll: data.poll })
-      })
-      .catch(error => {
-        console.error('Error fetching single poll', error)
-      })
+    const { _id, votedBy } = this.state.poll
+
+    if (!votedBy.includes(this.state.ip)) {
+      axios.post(`${baseURL}/polls/vote/${_id}`, { voter: this.state.ip, vote: this.state.selectedOption })
+        .then(({ data }) => {
+          this.setState({ poll: data.poll })
+        })
+        .catch(error => {
+          console.error('Error fetching single poll', error)
+        })
+    }
+    else {
+      this.setState({ showAlertModal: true })
+    }
+
   }
 
   renderPollOptions() {
@@ -79,6 +106,14 @@ export default class PollItem extends Component {
             </Panel>
           </Col>
         </Row>
+        <Modal show={this.state.showAlertModal} onHide={this.closeModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Already Voted!</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>This ip address has already voted on this poll.</p>
+          </Modal.Body>
+        </Modal>
       </Grid>
     )
   }
