7293a64e6c19c688b3d623d88dc81b71e1617e39
Stephen Mayeux <stephenmayeux@gmail.com>
2017-06-04 11:51:21 -0500

    twitter on the server

--- nightlife/server/server.js
+++ nightlife/server/server.js
@@ -1,8 +1,12 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const logger = require('morgan')
 const express = require('express')
+const passport = require('passport')
+
+require('./services/passport')(passport)
 
 const app = express()
 
@@ -11,6 +15,13 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nightlife')
 app.use(cors())
 app.use(logger('combined'))
 app.use(bodyParser.json({ type: '*/*' }))
+app.use(require('express-session')({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize())
+app.use(passport.session())
 
 const routes = require('./routes')
 app.use('/', routes)
