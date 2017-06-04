a41e8a5ecbba7f81bfebcb3532b4102769fce1a9
Stephen Mayeux <stephenmayeux@gmail.com>
2017-06-04 11:14:44 -0500

    initial commit

---
+++ nightlife/server/server.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+const express = require('express')
+
+const app = express()
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nightlife')
+
+app.use(cors())
+app.use(logger('combined'))
+app.use(bodyParser.json({ type: '*/*' }))
+
+const routes = require('./routes')
+app.use('/', routes)
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log('Running on port', port)
+})
