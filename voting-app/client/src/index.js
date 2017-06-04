351630559718da4f4a9789897a8fbbea6601e3a5
Stephen Mayeux <stephenmayeux@gmail.com>
2017-06-04 07:48:32 -0500

    rip out redux

--- voting-app/client/src/index.js
+++ voting-app/client/src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import reduxThunk from 'redux-thunk';
-import logger from 'redux-logger';
 
 import AuthService from './lib/AuthService'
 
-import App from './containers/App';
+import App from './components/App';
 import Home from './components/LandingPage'
 import PollList from './components/PollList'
 import PollItem from './components/PollItem'
 import CreatePollItem from './components/CreatePollItem'
 import MyPolls from './components/MyPolls'
-
-import reducers from './reducers';
 
 const auth = new AuthService('dcajj9RQ__rQAVVzbqp7hTALGeVv_FnZ', 'thebestjs.auth0.com')
 // eslint-disable-next-line
@@ -25,20 +19,14 @@ const requireAuth = (nextState, replace) => {
   }
 }
 
-const createStoreWithMiddleware = applyMiddleware(reduxThunk, logger)(createStore);
-const store = createStoreWithMiddleware(reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App} auth={auth}>
-        <IndexRoute component={Home} />
-        <Route path="/vote" component={PollList} />
-        <Route path="/vote/:id" component={PollItem} auth={auth} />
-        <Route path="/create" component={CreatePollItem} onEnter={requireAuth} auth={auth} />
-        <Route path="/mypolls" component={MyPolls} onEnter={requireAuth} auth={auth} />
-      </Route>
-    </Router>
-  </Provider>
+  <Router history={browserHistory}>
+    <Route path="/" component={App} auth={auth}>
+      <IndexRoute component={Home} />
+      <Route path="/vote" component={PollList} />
+      <Route path="/vote/:id" component={PollItem} auth={auth} />
+      <Route path="/create" component={CreatePollItem} onEnter={requireAuth} auth={auth} />
+      <Route path="/mypolls" component={MyPolls} onEnter={requireAuth} auth={auth} />
+    </Route>
+  </Router>
   , document.getElementById('root'));
--- voting-app/client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-const rootReducer = combineReducers({
-  state: (state = 'root reducer') => state
-});
-
-export default rootReducer;
