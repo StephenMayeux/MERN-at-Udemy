ea27132b0f65deecc30e71f07c604d0b32c01573
Stephen Mayeux <stephenmayeux@gmail.com>
2017-06-11 21:46:47 -0500

    make ui fixes

--- nightlife/client/src/components/BarCard/index.js
+++ nightlife/client/src/components/BarCard/index.js
@@ -3,26 +3,34 @@ import {
   Thumbnail,
   Button,
   Col,
-  Label
+  Label,
+  Glyphicon
 } from 'react-bootstrap'
+import _ from 'lodash'
 
 import './style.css'
 
 const BarCard = (props) => {
-  const { image_url, name, isLoggedIn, visitors, toggleCheckIn, id } = props
+  const { image_url, name, isLoggedIn, visitors, toggleCheckIn, id, user } = props
+  const buttonText = _.includes(visitors, user) ? 'Check Out' : 'Check In'
+  const indicator = _.includes(visitors, user)
+    ? <span><Glyphicon glyph="glass" /></span>
+    : null
+
   return (
     <Col xs={4}>
       <Thumbnail src={image_url}>
         <h3>{name}</h3>
         {isLoggedIn
           ? <span style={{ marginRight: 20 }}>
-              <Button onClick={() => toggleCheckIn(id)} bsStyle="primary">Check In</Button>
+              <Button onClick={() => toggleCheckIn(id)} bsStyle="primary">{buttonText}</Button>
             </span>
           : null
         }
         <span>
           <Label>{`${visitors.length} people are going`}</Label>
         </span>
+        {indicator}
       </Thumbnail>
     </Col>
   )
--- nightlife/client/src/containers/App/index.js
+++ nightlife/client/src/containers/App/index.js
@@ -108,6 +108,7 @@ class App extends Component {
             actions={actions}
             isLoggedIn={this.props.auth.isLoggedIn}
             toggleCheckIn={this.props.actions.toggleCheckIn}
+            user={this.props.auth.user}
           />
           {(i + 1) % 3 === 0
             ? <div className="clearfix visible-lg-block visible-md-block"></div>
