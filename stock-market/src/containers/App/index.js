b63da56c6314139c718e4bd7f98e2f48f273631c
Stephen Mayeux <stephenmayeux@gmail.com>
2017-06-24 23:37:44 -0500

    display line chart

--- stock-market/src/containers/App/index.js
+++ stock-market/src/containers/App/index.js
@@ -1,26 +1,75 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { Grid, Row, Col } from 'react-bootstrap'
+import {
+  LineChart,
+  XAxis,
+  YAxis,
+  Legend,
+  Line,
+  CartesianGrid,
+  Tooltip
+} from 'recharts'
 import io from 'socket.io-client'
+import _ from 'lodash'
 
+import { actionCreators as actions } from '../../actions'
 const socket = io.connect()
 
-export default class App extends Component {
-  constructor(props) {
-    super(props)
+const colors = [
+  '#82ca9d',
+  '#C0392B',
+  '#2980B9',
+  '#27AE60',
+  '#E67E22'
+]
 
-    this.state = {
-      symbols: null
-    }
-  }
+class App extends Component {
 
   componentDidMount() {
-    socket.on('symbols', (data) => {
-      console.log('these are symbols from server', data)
+    socket.on('init', this.props.actions.updateStocks)
+  }
+
+  renderChart() {
+    return (
+      <LineChart width={730} height={250} data={this.props.stocks.chartData}>
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Legend />
+        <Tooltip />
+        {this.renderChartLines()}
+      </LineChart>
+    )
+  }
+
+  renderChartLines() {
+    return this.props.stocks.tickers.map(ticker => {
+      return (
+        <Line key={ticker} type="monotone" dataKey={ticker} stroke={_.sample(colors)} />
+      )
     })
   }
 
   render() {
     return (
-      <h1>Hello World!</h1>
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            {this.props.stocks.tickers.length ? this.renderChart() : null}
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
+
+const mapStateToProps = ({ stocks }) => {
+  return { stocks }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return { actions: bindActionCreators(actions, dispatch) }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
