e9ef4982bbdc9d8080a0c5d65d0d9ef4cde16dda
Stephen Mayeux <stephenmayeux@gmail.com>
2017-06-25 16:18:49 -0500

    display ticker cards

---
+++ stock-market/src/components/TickerCard/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+  Glyphicon
+} from 'react-bootstrap'
+import './style.css'
+
+const TickerCard = (props) => {
+  return (
+    <div className="tickerCardWrapper">
+      <Glyphicon glyph="remove" className="pull-right" />
+      <h2>{props.ticker}</h2>
+      <p className="lead">{props.mostRecentClosePrice}</p>
+    </div>
+  )
+}
+
+export default TickerCard
--- stock-market/src/containers/App/index.js
+++ stock-market/src/containers/App/index.js
@@ -3,8 +3,10 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { Grid, Row, Col } from 'react-bootstrap'
 import io from 'socket.io-client'
+import _ from 'lodash'
 
 import LineChart from '../../components/LineChart'
+import TickerCard from '../../components/TickerCard'
 import { actionCreators as actions } from '../../actions'
 
 import './style.css'
@@ -17,20 +19,45 @@ class App extends Component {
     socket.on('init', this.props.actions.updateStocks)
   }
 
+  renderChart() {
+    if (this.props.stocks.tickers.length) {
+      return (
+        <LineChart
+          chartData={this.props.stocks.chartData}
+          tickers={this.props.stocks.tickers}
+        />
+      )
+    }
+    return <h2>Loading Data...</h2>
+  }
+
+  renderTickerCards() {
+    const mostRecentPrices = _.last(this.props.stocks.chartData)
+
+    return this.props.stocks.tickers.map(ticker => {
+      return (
+        <Col xs={4}>
+          <TickerCard
+            key={ticker}
+            mostRecentClosePrice={mostRecentPrices[ticker]}
+            ticker={ticker}
+          />
+        </Col>
+      )
+    })
+  }
+
   render() {
     return (
       <Grid>
         <Row>
           <Col xs={12}>
             <h1 style={{ textAlign: "center" }}>Stock Market App</h1>
-            {this.props.stocks.tickers.length
-              ? <LineChart
-                  className="chartWrapper"
-                  chartData={this.props.stocks.chartData}
-                  tickers={this.props.stocks.tickers}
-                />
-              : null}
+            {this.renderChart()}
           </Col>
+        </Row>
+        <Row>
+          {this.renderTickerCards()}
         </Row>
       </Grid>
     )
